94191d2cfe98b1fc58f6a2eb4a867e570c1710b6
Eldstrale <calle.eld@hotmail.com>
2020-05-18 11:46:33 +0200

    Racketen färdigställda

--- Pong/src/Pong/paddle.java
+++ Pong/src/Pong/paddle.java
@@ -11,4 +11,20 @@ public class paddle extends Rectangle {
 		setWidth(10);
 		setFill(Color.WHITE);
 
+	}
+
+	// flyttar racketen upp�t tills de börjar l�mna rutan
+	public void up() {
+		if (!getBoundsInParent().intersects(0, 0, 800, 0)) {
+			setTranslateY(getTranslateY() - 5);
+		}
+
+	}
+
+	// flyttar racketen ner�t tills de börjar l�mna rutan
+	public void down() {
+		if (!getBoundsInParent().intersects(0, 500, 800, 0)) {
+			setTranslateY(getTranslateY() + 5);
+		}
+	}
 }
